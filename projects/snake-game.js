0323c553b513d70c5887b6c8c1ae29425a0fc4f9
Lucas Hetrick <115331483+lucashetrick@users.noreply.github.com>
2023-07-09 21:44:33 -0700

    Update snake-game.js

--- projects/snake-game.js
+++ projects/snake-game.js
@@ -1,1 +1,9 @@
-
+function do () {
+  
+  let input = prompt("Enter Access Key. If you do not have the access key, you will have to request it.");
+      let p = "Iluvkittysqaud.me@248!"
+if(input==p)
+{} else {
+  window.location="./projects"
+}
+}
